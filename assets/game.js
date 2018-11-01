5cb23627fa030a0ce563ddc1405c3b3a25128662
Tim Brockman <tim@timbrockman.com>
2018-11-01 12:16:08 -0400

    fixed tabs

--- assets/game.js
+++ assets/game.js
@@ -1,12 +1,4 @@
 window.onload = ()=>{
-// Check for support
-//
-if (!ROT.isSupported()){
-	alert("the rot.js library isn't supported by your browser.");
-} else{
-	// ok
-	
-
 // create an 80 x 40 canvas
 const display = new ROT.Display({width:80, height:40});
 const container = display.getContainer();
@@ -14,14 +6,13 @@ document.body.appendChild(Container);
 
 let foreground, background, colors;
 for (let i = 0; i<15; i++){
-	foreground = ROT.Color.toRGB([
-		255 - (i*20),
-		255 - (i*20),
-		255 - (i*20)
-	]);
-	background = ROT.Color.toRGB([i*20, i*20, i*20]);
-	colors = `%c{${foreground}}%b${background}}`;
-	display.drawText(2, i, colors + "Hello, world!");
-}
+  foreground = ROT.Color.toRGB([
+    255 - (i*20),
+    255 - (i*20),
+    255 - (i*20)
+  ]);
+  background = ROT.Color.toRGB([i*20, i*20, i*20]);
+  colors = `%c{${foreground}}%b${background}}`;
+  display.drawText(2, i, colors + "Hello, world!");
 }
 }
